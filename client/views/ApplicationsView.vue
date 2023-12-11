c5d6bb067f3722965e8cc3b781a8cefb5ea7ff7e
Jennifer Fairhurst <jennifer.dakkak@gmail.com>
2023-12-11 11:59:42 -0500

    stupid vue error fixed by refreshing vscode

--- client/views/ApplicationsView.vue
+++ client/views/ApplicationsView.vue
@@ -1,7 +1,28 @@
-<script></script>
-
+<script setup lang="ts">
+import RegisterForm from "@/components/Login/RegisterForm.vue";
+</script>
 <template>
-  <div>
-    <h1>hi</h1>
-  </div>
+  <main>
+    <div>
+      <RegisterForm />
+      <h1>Page Not Found</h1>
+      <p>The page that you requested could not be found.</p>
+      <RouterLink to="/" class="button"> Return to the home page </RouterLink>
+    </div>
+  </main>
 </template>
+<style scoped>
+div {
+  min-width: 100%;
+  min-height: 100%;
+  padding-top: 15%;
+  text-align: center;
+}
+p {
+  margin-bottom: 30px;
+}
+.button {
+  color: #d77533;
+  border-color: #d77533;
+}
+</style>
